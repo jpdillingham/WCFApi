2ad74e4267b99d4fd1954dc3d2fd5b14e8431dad
JP Dillingham <jpdillingham@gmail.com>
2018-09-22 09:30:13 -0500

    initial commit

---
+++ WCFApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WCFApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:80";
+            var host = new WebServiceHost(typeof(Service), new Uri(url));
+            var endpoint = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "");
+            host.Open();
+
+            Console.WriteLine($"Api hosted at {url}.  Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        string HelloWorld();
+    }
+
+    public class Service : IService
+    {
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ WCFApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WCFApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WCFApi")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d58d4f6-dc1f-4b08-b6a2-dd443ae51796")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
